c6c38549e0e395a87d90b46835a111a44e6ff643
AntonVI86 <lionter0@gmail.com>
2022-08-11 14:54:40 +0300

    Initial commit

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private float _maxHealth;
+
+    private float _currentHealth;
+
+    public float MaxHealth => _maxHealth;
+    public float CurrentHealth => _currentHealth;
+
+    private void Awake()
+    {
+        _currentHealth = _maxHealth;
+    }
+
+    public void TakeDamage(float damage) 
+    {
+        _currentHealth -= damage;
+
+        if (_currentHealth > _maxHealth)
+        {
+            _currentHealth = _maxHealth;
+        }
+        else if (_currentHealth < 0) 
+        {
+            _currentHealth = 0;
+        }
+    }    
+}
---
+++ Assets/Scripts/HealthDisplaying.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthDisplaying : MonoBehaviour
+{
+    [SerializeField] private Slider _healthBar;
+    [SerializeField] private Health _playerHealth;
+    [SerializeField] private float _fillSpeed;
+    [SerializeField] private Image _fill;
+    [SerializeField] private Color _startColor;
+    [SerializeField] private Color _finishColor;
+
+    private void Awake()
+    {
+        _healthBar.maxValue = _playerHealth.MaxHealth;
+    }
+
+    public void OnChangedHealthValue() 
+    {
+        StartCoroutine(ChangeHealthValue());
+        StartCoroutine(ChangeColor());
+    }
+
+    private IEnumerator ChangeColor() 
+    {
+        float normValue = _playerHealth.CurrentHealth / _playerHealth.MaxHealth;
+        _fill.color = Color.Lerp(_finishColor, _startColor, normValue);
+        yield return null;
+    }
+
+    private IEnumerator ChangeHealthValue() 
+    {
+        while (_healthBar.value != _playerHealth.CurrentHealth) 
+        {            
+            _healthBar.value = Mathf.MoveTowards(_healthBar.value, _playerHealth.CurrentHealth, _fillSpeed * Time.deltaTime);           
+            yield return null;
+        }
+    }
+}
